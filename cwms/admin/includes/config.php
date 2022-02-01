0ca2e958abe354348cd3e96c62bc45fc62c170d7
muni1430 <93640586+muni1430@users.noreply.github.com>
2022-02-01 09:49:33 +0530

    Update config.php

--- cwms/admin/includes/config.php
+++ cwms/admin/includes/config.php
@@ -1,8 +1,8 @@
 <?php 
 // DB credentials.
-define('DB_HOST','localhost');
-define('DB_USER','root');
-define('DB_PASS','');
+define('DB_HOST','myrds.cxnn8acptt6e.us-east-1.rds.amazonaws.com:3306');
+define('DB_USER','admin');
+define('DB_PASS','muni1234');
 define('DB_NAME','cwmsdb');
 // Establish database connection.
 try
